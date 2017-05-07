69b1b32aa3c18a370c092222637817ccbc97d700
starpx <starpx000@gmail.com>
2017-05-07 00:46:09 -0300

    Se sube clase que permite Rotar Numeros.

---
+++ src/sandbox_java/RotaNumeros.java
@@ -0,0 +1,26 @@
+package sandbox_java;
+
+//Se muestra como se implementa una rotacion tipica hacia la izquierda o 
+//derecha de una cifra de numeros.
+public class RotaNumeros {
+
+	public int rotaIzquierda(){
+		int res=0;
+		return res;
+	}
+	
+	public int rotaDerecha(){
+		int res=0;
+		return res;
+	}
+	
+	public void escribe(){
+		
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
